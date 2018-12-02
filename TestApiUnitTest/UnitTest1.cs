d010e9430a038052b52339b0bea8cca87714a991
Joachim Halvorsen <joachimhal@gmail.com>
2018-12-02 17:22:45 +0100

    Added solutionfile and removed failing test

--- TestApiUnitTest/UnitTest1.cs
+++ TestApiUnitTest/UnitTest1.cs
@@ -12,11 +12,5 @@ namespace TestApiUnitTest
             var a = new List<string>();
             Assert.Equal(4, (int) Math.Round(4.0));
         }
-
-        [Fact]
-        public void Failingest()
-        {
-            Assert.Equal(4, 5);
-        }
     }
 }
