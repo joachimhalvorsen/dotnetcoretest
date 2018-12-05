5f1fdf46b3b789a70d46ff7eb7268e58ae3d8a27
Joachim Halvorsen <joachimhal@gmail.com>
2018-12-05 20:02:26 +0100

    added task for building solution

--- TestApi/Entities/PerformanceTestDBContext.cs
+++ TestApi/Entities/PerformanceTestDBContext.cs
@@ -18,15 +18,6 @@ namespace TestApi.Entities
         public virtual DbSet<Email> Email { get; set; }
         public virtual DbSet<EmailAttachment> EmailAttachment { get; set; }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            if (!optionsBuilder.IsConfigured)
-            {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Server=tcp:jhperformancetest.database.windows.net,1433;Initial Catalog=PerformanceTestDB;Persist Security Info=False;User ID=jh;Password=ad37kWEw2qM9;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
-            }
-        }
-
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Email>(entity =>
